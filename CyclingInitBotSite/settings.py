88962eb5929c04a7d35b681535892a1d4c20990d
psemdel <maxime.delzenne@gmail.com>
2020-09-09 13:57:20 +0200

    Debug false

--- CyclingInitBotSite/settings.py
+++ CyclingInitBotSite/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-DEBUG=True
+DEBUG=False
 
 if DEBUG:
     with open('CyclingInitBotSite/etc/secret_key.txt') as f:
