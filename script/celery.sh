fb2260f0b443b52c4954bc8b26f76263af16a73f
psemdel <maxime.delzenne@gmail.com>
2020-10-02 15:14:49 +0200

    fix user

--- script/celery.sh
+++ script/celery.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Starting celery"
-/data/project/cycling-init-bot/www/python/venv/bin/python3 /data/project/cycling-init-bot/www/python/venv/bin/celery -A CyclingInitBotSite worker -l info
+/data/project/cycling-init-bot/www/python/venv/bin/python3 /data/project/cycling-init-bot/www/python/venv/bin/celery -A /data/project/cycling-init-bot/www/python/CyclingInitBotSite worker -l info
 echo "Celery started"
--- users/models.py
+++ users/models.py
@@ -7,7 +7,7 @@ from sentry_sdk import capture_message
 class UserProfile(models.Model):
     user=models.OneToOneField(User, on_delete=models.CASCADE)
     wiki_name = models.CharField(max_length=100,null=False, blank=False)
-    capture_message("user " + user + " created")
+    capture_message("user " + user.username + " created")
     
     def __str__(self):
         return self.user.username   
