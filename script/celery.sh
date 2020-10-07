2ec31f359676cf60d2216a914195774712b52d62
psemdel <maxime.delzenne@gmail.com>
2020-10-07 17:16:43 +0200

    script that run

--- script/celery.sh
+++ script/celery.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-echo "Starting celery"
-/data/project/cycling-init-bot/www/python/venv/bin/python3 /data/project/cycling-init-bot/www/python/venv/bin/celery -A /data/project/cycling-init-bot/www/python/CyclingInitBotSite worker -l info
-echo "Celery started"
+/data/project/cycling-init-bot/www/python/venv/bin/celery -A CyclingInitBotSite worker -l info
