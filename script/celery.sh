bd6744a507c7f0eb94b9226d6eed7d2b245db35d
psemdel <maxime.delzenne@gmail.com>
2021-11-14 15:14:07 +0100

    new python image for celery

--- script/celery.sh
+++ script/celery.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cd /data/project/cycling-init-bot/www/python/src
+source $HOME/www/python/venv/bin/activate
+
 #/data/project/cycling-init-bot/venv_celery/bin/celery -A CyclingInitBotSite worker -l info -P solo
 /data/project/cycling-init-bot/www/python/venv/bin/celery -A CyclingInitBotSite worker -l info -P solo
