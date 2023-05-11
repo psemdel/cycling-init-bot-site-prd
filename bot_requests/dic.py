7cb949e0826eea056d3047daf9827ba978e55ddd
psemdel <maxime.delzenne@gmail.com>
2023-05-11 21:16:33 +0200

    typo

--- bot_requests/dic.py
+++ bot_requests/dic.py
@@ -21,7 +21,7 @@ def routine_to_model(routine):
      "sort_name":SortNameRequest,
      "UCIranking":UCIrankingRequest,
      "start_list":StartListRequest,
-     "team_importer":TeamImporterRequest,
+     "team_importer":TeamImporterRequest,  
      "national_all_champs":NationalAllChampsRequest,
      "national_one_champ":NationalOneChampRequest
      }
@@ -44,7 +44,7 @@ def routine_to_serializer(routine):
      "sort_name":SortNameRequestSerializer,
      "UCIranking":UCIrankingRequestSerializer,
      "start_list":StartListRequestSerializer,
-     "team_importer":TeamImportRequestSerializer,
+     "team_importer":TeamImporterRequestSerializer, 
      "national_all_champs":NationalAllChampsRequestSerializer,
      "national_one_champ":NationalOneChampRequestSerializer
      }
